286ab3310098725558c52fe3c81c40a6c7e54ead
satish <satishpunekar27@gmail.com>
2025-08-18 00:22:42 +0530

    commit

--- src/test/java/Steps/Login.java
+++ src/test/java/Steps/Login.java
@@ -1,11 +1,22 @@
 package Steps;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class Login {
 @Given("user open the application")
     public void open_application(){
-    System.out.println("Satish raje");
+    System.out.println("Given");
 
 }
+@When("user enters the username and password")
+    public void enter_username_password(){
+    System.out.println("when");
+}
+
+@Then("user should login successfully")
+    public void login_success(){
+        System.out.println("Then");
+    }
 }
